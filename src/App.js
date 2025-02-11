dcef3acc1ad40124a24008c32085a6b2b4d941dc
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-11 15:40:32 -0500

    HTTP request from App.js to update the data in the DB through PUT method.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
@@ -6,6 +8,15 @@ import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-htt-34fa6-default-rtdb.firebaseio.com/cart.json", {
+      // Con el PUT los datos no se agregaran en una lista de datos, sino que sobreescribiran los datos existentes.
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
