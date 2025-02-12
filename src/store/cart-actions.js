1b1be15102e6988e09fe47c8e788f2e7087c192e
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-12 10:29:42 -0500

    Fetching data to show the cart items once the page loads for the first time.

---
+++ src/store/cart-actions.js
@@ -0,0 +1,83 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-htt-34fa6-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching cart data failed!",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  // retorno otra funcion. Si, en js puedo crear funciones que retornen funciones.
+  return async (dispatch) => {
+    // Pendiente
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-htt-34fa6-default-rtdb.firebaseio.com/cart.json",
+        {
+          // Con el PUT los datos no se agregaran en una lista de datos, sino que sobreescribiran los datos existentes.
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+    try {
+      await sendRequest();
+
+      // Exito
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      // Error
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,13 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import { uiActions } from "./ui-slice";
-
 const initialState = { items: [], totalQuantity: 0 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -38,60 +40,6 @@ const cartSlice = createSlice({
     },
   },
 });
-
-// despues del objeto slice.
-
-export const sendCartData = (cart) => {
-  // retorno otra funcion. si, en js puedo crear funciones que retornen funciones.
-  return async (dispatch) => {
-    // Pendiente
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        "https://react-htt-34fa6-default-rtdb.firebaseio.com/cart.json",
-        {
-          // Con el PUT los datos no se agregaran en una lista de datos, sino que sobreescribiran los datos existentes.
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-    };
-    try {
-      await sendRequest();
-
-      // Exito
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    } catch (error) {
-      // Error
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    }
-  };
-};
-// no estoy ejecutando este codigo en un reducer, por lo que aqui pueden haber efectos secundarios
-// y codigo asincrono.
 
 export const cartActions = cartSlice.actions;
 
